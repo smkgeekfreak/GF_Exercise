771f654a8233490a1522ee71d47c3f08dba35ff2
Shawn Kircher <smkgeekfreak@gmail.com>
2019-10-11 11:43:54 -0500

    added try /catch fixed failing mocha spec

--- repo/writeRepo.mocha.spec.js
+++ repo/writeRepo.mocha.spec.js
@@ -4,17 +4,20 @@ const Logger = require('../logger')("model-test",process.env.PHOENIX_GATEWAY_TES
 const writeRepo = require('./writeRepo');
 
 describe('writeRepo Spec', () => {
+  //TODO: skip tests until providing a mock/faker
   it('Load Model', async () => {
     const eemongo = require('./employeeWriterDB');
     const eeWriter = new eemongo(Logger, "mongodb://localhost:32768/newtest");
-    await eeWriter.create(      {
+    const response = await eeWriter.create(      {
       email:'shawn@hired.com',
       id:' d643e381-df8c-43b2-844e-d816baca5828',
       role:'new employee',
       telephone: '615-540-4550',
-      lastName:  'shawn',
-      firstName:'kirch'
+      lastName:  'shawn ' + Math.random(),
+      firstName:'kirch '
     });
+    Logger.debug (response);
+    expect(response.statusCode).to.equal('success');
   })
   it.skip('Load Model', async () => {
     const wRepo = new writeRepo({writeModel:new (require('./employeeWriterDB'))(Logger)});
@@ -29,7 +32,7 @@ describe('writeRepo Spec', () => {
       });
 
     Logger.debug(result)
-    expect(result).to.equal('success');
+    expect(result.statusCode).to.equal('success ');
 //    expect(await wRepo.create({name:"is"})).to.equal('success');
 //    expect(await wRepo.create({name:"the"})).to.equal('success');
 //    expect(await wRepo.create({name:"next"})).to.equal('success');
